c5245bc2cd9d720a657a255b786f3a35e84b7480
Bruno <dronecraftbr10@gmail.com>
2022-10-02 17:47:05 -0300

    Commit final. Fácil de mais XD

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("1234567", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("22222222", "Modelo 1", "2222222222", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
